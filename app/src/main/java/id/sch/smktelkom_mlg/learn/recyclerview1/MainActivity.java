51d6cec18588100e9474667c6a7fe7505ab0e091
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-11-14 07:20:07 +0700

    RecyclerView List Round Image

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
@@ -41,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
         Drawable[] arFoto = new Drawable[a.length()];
         for (int i = 0; i < arFoto.length; i++) {
-            arFoto[i]=a.getDrawable(i);
+            BitmapDrawable bd = (BitmapDrawable) a.getDrawable(i);
+            RoundedBitmapDrawable rbd =
+                    RoundedBitmapDrawableFactory.create(getResources(), bd.getBitmap());
+            rbd.setCircular(true);
+            arFoto[i] = rbd;
+
+
         }
         a.recycle();
         for (int i = 0; i < arJudul.length; i++) {
